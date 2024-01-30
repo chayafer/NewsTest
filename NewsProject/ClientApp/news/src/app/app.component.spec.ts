470a73b66f758bed09d8cec8688144fe234867c6
chayafer <chayafer@gmail.com>
2024-01-30 10:25:14 +0200

    add ClientApp

--- NewsProject/ClientApp/news/src/app/app.component.spec.ts
+++ NewsProject/ClientApp/news/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'news-frontend' title`, () => {
+  it(`should have the 'news' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('news-frontend');
+    expect(app.title).toEqual('news');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, news-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, news');
   });
 });
